6228678a83fbb2569d1781f5b4db07563cdc1aa2
Otávio Augusto Dellay Secco <otavio.secco@estudante.uffs.edu.br>
2022-12-27 11:48:49 -0300

    Fix wine.csv " ,. " breaking standard
    AAAAAAAAAAAAAAAAAAAAAAAAAA

--- Assets/ReadCSV.cs
+++ Assets/ReadCSV.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using UnityEngine;
 using System.Linq; 
+using System.Globalization;
 
 public class ReadCSV : MonoBehaviour{
+
     public TextAsset csvFile; 
+
 	public void Start(){
 		string[,] grid = SplitData(csvFile.text);
+		PlaceCubes(grid);
+	}
+	
+	public void PlaceCubes(string[,] grid){
+
+		int linSize = grid.GetLength(0);
+		int colSize = grid.GetLength(1);
+
+		for(int z = 1; z < colSize -1 ; z++){
+
+			double porcodio = double.Parse(grid[2,z]);
+			Debug.Log( z + " " + porcodio);
+		}
 	}
 	
 	static public string[,] SplitData(string csvText){
 		string[] lines = csvText.Split("\n"[0]); 
 		int width = 0; 
-		string debugaa = "";
 		for (int i = 0; i < lines.Length; i++){
 			string[] row = SplitCsvLine( lines[i] ); 
 			width = Mathf.Max(width, row.Length); 
@@ -19,13 +34,11 @@ public class ReadCSV : MonoBehaviour{
 		string[,] outputGrid = new string[width + 1, lines.Length + 1]; 
 		for (int y = 0; y < lines.Length; y++){
 			string[] row = SplitCsvLine( lines[y] ); 
-			debugaa += outputGrid[0,y] + " ";
 			for (int x = 0; x < row.Length; x++) {
 				outputGrid[x,y] = row[x]; 
 				outputGrid[x,y] = outputGrid[x,y].Replace("\"\"", "\"");
 			}
 		}
-		Debug.Log(debugaa);
 		return outputGrid; 
 	}
  
